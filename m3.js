5d07565566f2708a6ad159e409ea0888dddbcb16
William Becker <will@qubitdigital.com>
2011-08-05 08:58:56 +0100

    Keep waiting until element exists, before you put something in it

--- m3.js
+++ m3.js
@@ -4,32 +4,43 @@ document.write = function (t) {
   text.push(t);
 }
 var i = 0, ii = s.length;
-var doNext = function () {
-  var el = document.createElement("script");
-  el.src=s[i];
-  el.onload = function () {
-    var c, p; 
-    try {
-      p = pos;
-      p = pos.split(",").length > i ? pos.split(",")[i] : pos.split(",")[0];
-    } catch (e1) {
-      try {
-        p = pos.split(",")[0];
-      } catch (e2) {
-        p = null;
-      }
-    }
-    if (p) {
-      c = document.getElementById(p);
+var addText = function (elName) {
+  var f = function () {
+    var c;
+    if (elName) {
+      c = document.getElementById(elName);
     } else {
       c = document.body;
     }
-    c.innerHTML += text.join("\n");
-    text=[];
-    i++;
-    if (i < ii) {
-      doNext();
+    if (c) {
+      c.innerHTML += text.join("\n");
+      text=[];
+      i++;
+      if (i < ii) {
+        doNext();
+      }
+    } else {
+      setTimeout(f, 10);
     }
+  };
+  return f;
+}
+var doNext = function () {
+  var p; 
+  try {
+    p = pos;
+    p = pos.split(",").length > i ? pos.split(",")[i] : pos.split(",")[0];
+  } catch (e1) {
+    try {
+      p = pos.split(",")[0];
+    } catch (e2) {
+      p = null;
+    }
+  }
+  var el = document.createElement("script");
+  el.src=s[i];
+  el.onload = function () {
+    addText(p)();
   }
   document.getElementsByTagName("head")[0].appendChild(el);
 }
